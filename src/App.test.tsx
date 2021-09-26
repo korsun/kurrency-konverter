cf95dfaaa1f067ce409e74b4013ce7512d0cd334
Kirill Korsun <k.korsun@tinkoff.ru>
2021-09-26 08:29:42 +0300

    add some test scenarios

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('normal work', () => {
+	it('fetches rates and puts them into the state');
+	it('renders elements with their values');
+	it('switches the accounts and displays it on the page');
+	it('converts currency on input change');
+	it('trades currencies between the accounts');
+	it('switches [from] and [to] fields on arrow click');
+});
+
+describe('crash tests', () => {
+	it('validates the input');
+	it('shows "exceeds balance" when amount to trade is higher then availability');
+	it('handles onfetch error');
 });
