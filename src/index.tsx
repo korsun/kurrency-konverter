cd8c3168c4336395c370de38124a37e26e0a8c32
Kirill Korsun <k.korsun@tinkoff.ru>
2021-09-28 09:52:57 +0300

    index and head tag cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
